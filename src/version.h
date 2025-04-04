595fa3582c42f3db929b23af5eacb79b39143ee1
nia <nia@NetBSD.org>
2025-04-04 14:07:34 +0200

    special versions for 3ds

--- src/version.h
+++ src/version.h
@@ -2,10 +2,14 @@
 #define _H_VERSION
 
 #define VERSION_CONFIG 85
+#if defined(ANDROID) || defined(_3DS)
+#define VERSION_ENTITY 24
+#else
 #define VERSION_ENTITY 8
+#endif
 #define VERSION_FILTER 2
 #define VERSION_MAPS 27
-#ifdef ANDROID
+#if defined(ANDROID) || defined(_3DS)
 #define VERSION_MEDIA 59
 #else
 #define VERSION_MEDIA 26
